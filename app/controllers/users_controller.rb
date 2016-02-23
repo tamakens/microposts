0f3d7285a080672bac4df8cbbe7ecf7cb0d4ac15
tamakens <kengotamai@gmail.com>
2016-02-23 21:52:34 +0000

    tmp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :authenticate!, only: [:edit, :update]
+
   def show
-   @user = User.find(params[:id])
-   @microposts = @user.microposts.order(created_at: :desc)
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
-        @user = User.new
+    @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,20 +21,45 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "修正しました！"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   
   def followings
-     @user = User.find(params[:follow_id])
+    @user = User.find(params[:follow_id])
   end
   
   def followers
-     @user = User.find(params[:followed_id])
+    @user = User.find(params[:followed_id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
+
+  def user_profile
+    params.require(:user).permit(:name, :email, :profile, :area, :birthday, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authenticate!
+    if @user != current_user
+      redirect_to root_url, flash: { dander: "不正なアクセス" }
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ActiveRecord::Base
-    before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-    has_secure_password
-    has_many :microposts
-    has_many :following_relationships, class_name:  "Relationship",
-    foreign_key: "follower_id", dependent: :destroy
-    has_many :following_users, through: :following_relationships, source: :followed
+
+  has_secure_password
+
+  has_many :microposts
+  has_many :following_relationships, class_name:  "Relationship",
+  foreign_key: "follower_id", dependent: :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
 
       # 他のユーザーをフォローする
   def follow(other_user)
