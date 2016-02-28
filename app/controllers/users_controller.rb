56bc9cb9d70c4f6a30741f5fe1821edd3843b77f
tamakens <kengotamai@gmail.com>
2016-02-28 03:31:41 +0000

    region

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :region,:password, :password_confirmation)
   end
 
   def user_profile
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,7 @@ ActiveRecord::Schema.define(version: 20160217121825) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "user"
-    t.integer  "region"
+    t.string   "region"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
